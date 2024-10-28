40d34776b0208354391d00f5c298b0af92b47d78
elziorocha <rochasenzo.04@gmail.com>
2024-10-28 11:41:13 -0300

    feat:implementação do código frontend no projeto compartilhado no GIt

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,69 @@
-<script setup>
-import HelloWorld from '@/components/HelloWorld.vue';
-
-</script>
-
 <template>
-  <main>
-    <HelloWorld msg="Olá!" />
+  <main class="px-10 py-5 flex flex-col justify-between gap-10 bg-cover">
+    <div class="flex justify-between items-center">
+      <div class="flex gap-4 items-center">
+        <img src="../assets/images/logo.png" alt="Logo da página" class="w-20 drop-shadow-imageShadow1" />
+        <h1 class="text-zinc-200 text-5xl font-semibold drop-shadow-imageShadow1 italic">
+          Pampa's RH
+        </h1>
+      </div>
+      <nav class="flex gap-8 items-center font-bold text-xl">
+        <RouterLink to="/about" class="navHover">Sobre nós</RouterLink>
+        <RouterLink to="/contact" class="navHover">Contato</RouterLink>
+        <RouterLink to="/recruiter-login" class="navHover">Seja um recrutador</RouterLink>
+        <RouterLink to="/login"
+          class="px-5 py-2.5 bg-lightBlue rounded-full hover:bg-darkBlue transition-all duration-150 shadow-xl flex gap-1">
+          Login
+          <User stroke-width="3px" />
+        </RouterLink>
+      </nav>
+    </div>
+    <section class="w-full flex gap-72 h-[30rem]">
+      <div class="mt-16 w-7/12 flex flex-col gap-4">
+        <h1 class="text-title font-bold drop-shadow-textShadow2">
+          Seja Bem-Vindo(a)!!!
+        </h1>
+        <h2 class="w-10/12 font-medium text-xl drop-shadow-textShadow">
+          Nós somos a Pampa's RH, o melhor sistema de gerenciamento e procura de
+          vagas disponível no mercado!
+        </h2>
+        <div class="flex items-center justify-start text-lg gap-4">
+          <RouterLink to="/login"
+            class="px-5 py-2.5 bg-lightBlue rounded-full hover:bg-darkBlue transition-all duration-200 shadow-md font-semibold flex gap-1">
+            <User stroke-width="3px" />
+            Login
+          </RouterLink>
+          <RouterLink to="/recruiter-login"
+            class="px-5 py-2.5 rounded-full transition-all duration-200 shadow-md font-semibold border-2 border-zinc-300 hover:bg-secondaryColor hover:border-sky-900">
+            Contrate agora
+          </RouterLink>
+        </div>
+      </div>
+      <div class="drop-shadow-imageShadow2">
+        <img src="../assets/images/HomeImage1.jpg" alt=""
+          class="clip-home home-image relative right-24 bottom-5 z-10" />
+        <div class="job-icons bottom-[28rem] right-28">
+          <FileUser color="#ffd91a" size="56px" />
+        </div>
+        <div class="job-icons bottom-96 left-48">
+          <Search color="#e60000" size="56px" />
+        </div>
+        <div class="job-icons bottom-72 right-14">
+          <BriefcaseBusiness color="#000" size="56px" />
+        </div>
+      </div>
+    </section>
+    <footer class="flex flex-col items-center justify-center">
+      <p>2024 | ©Copyright for the Developers in this project</p>
+      <div class="flex gap-2">
+        <p>Enzo Rocha</p>
+        -
+        <p>Tiago Remes</p>
+      </div>
+    </footer>
   </main>
 </template>
+
+<script setup>
+import { User, FileUser, Search, BriefcaseBusiness } from 'lucide-vue-next'
+</script>
---
+++ frontend/src/views/UserAuth/UserLoginView.vue
@@ -0,0 +1,70 @@
+<template>
+  <main class="flex w-full">
+    <section class="h-screen text-slate-950 flex flex-col gap-7 w-4/6 px-10 py-5">
+      <div class="flex gap-4 items-center">
+        <img src="../../assets/images/logo.png" alt="Logo da página" class="w-20 invert" />
+        <h1 class="text-5xl font-bold italic">Pampa's RH</h1>
+      </div>
+      <div class="italic font-medium">
+        <h3 class="text-2xl font-bold">Bem-vindo ao Pampa's RH Enterprises</h3>
+        <h4 class="text-lg">
+          Acesse ou crie a sua conta para gerenciar suas candidaturas e
+          oportunidades.
+        </h4>
+      </div>
+
+      <div class="flex items-center justify-center">
+        <form action="" class="w-9/12 flex flex-col gap-10">
+          <div class="flex items-center border-b-2 border-black">
+            <Mail class="ml-2" />
+            <input type="email" class="input1" placeholder="Insira seu email" />
+          </div>
+
+          <div class="flex items-center border-b-2 border-black">
+            <LockKeyhole class="ml-2" />
+            <input type="password" class="input1" placeholder="Insira sua senha" />
+          </div>
+
+          <div class="flex justify-between items-center">
+            <div class="flex flex-col gap-1">
+              <p>Não tem uma conta ainda?</p>
+              <div class="flex gap-1 group">
+                <RouterLink to="/register" class="underline underline-offset-4 hover:text-secondaryColor">
+                  <p>Crie agora mesmo!</p>
+                </RouterLink>
+                <ArrowRight class="size-5 self-center group-hover:translate-x-2 transition-all duration-150" />
+              </div>
+              <div class="flex gap-1 group">
+                <RouterLink to="/recruiter-login" class="underline underline-offset-4 hover:text-secondaryColor">
+                  <p>É um recrutador? Entre aqui!</p>
+                </RouterLink>
+                <ArrowRight class="size-5 self-center group-hover:translate-x-2 transition-all duration-150" />
+              </div>
+            </div>
+            <button
+              class="px-5 py-2.5 hover:text-zinc-300 bg-lightBlue rounded-[100px] hover:bg-primaryColor transition-all items-center shadow-xl font-bold hover:rounded-xl">
+              Entrar
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div class="flex font-bold text-lg absolute bottom-6 left-6">
+        <RouterLink to="/"
+          class="pl-3 pr-4 py-2.5 hover:text-zinc-300 bg-lightBlue rounded-[100px] hover:bg-primaryColor transition-all items-center duration-150 shadow-xl flex gap-0.5 hover:rounded-xl">
+          <ArrowLeft />
+          Home
+        </RouterLink>
+      </div>
+    </section>
+
+    <section class="h-screen bg-secondaryColor w-full bg-login-cover flex items-center justify-center">
+      <img src="../../assets/images/Interview1.jpg" alt="Foto de entrevista na página de login"
+        class="w-[36rem] h-[28rem] rounded-2xl shadow-shadow1 bg-contain bg-center" />
+    </section>
+  </main>
+</template>
+
+<script setup>
+import { ArrowLeft, ArrowRight, LockKeyhole, Mail } from 'lucide-vue-next'
+</script>
