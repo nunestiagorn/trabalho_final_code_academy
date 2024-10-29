d7cf8d2bfbf9be27483912abee79216af5c9f505
nunestiagorn <nunestiagorn@gmail.com>
2024-10-29 11:43:09 -0300

    feat:Adionado Modelos e Controladores de sistema de autenticação.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,19 +1,23 @@
 <?php
 
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\CompaniesController;
+use App\Http\Controllers\JobOpeningsController;
+use App\Http\Controllers\ApplicationsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('users', [UsersController::class, "index"]);
+Route::post('users', [UsersController::class, "store"]);
+Route::put('users/{id}', [UsersController::class, "update"]);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::get('companies', [CompaniesController::class, "index"]);
+Route::post('companies', [CompaniesController::class, "store"]);
+Route::put('companies/{id}', [CompaniesController::class, "update"]);
+
+Route::get('job_openings', [JobOpeningsController::class, "index"]);
+Route::post('job_openings', [JobOpeningsController::class, "store"]);
+Route::put('job_openings/{id}', [JobOpeningsController::class, "update"]);
+
+Route::get('applications', [ApplicationsController::class, "index"]);
