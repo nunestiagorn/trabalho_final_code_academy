653f2ce0ff77770d9f8ccf12d322d1a7e0385e09
nunestiagorn <nunestiagorn@gmail.com>
2024-10-30 11:08:45 -0300

    feat:resgate do usuário do banco com sucesso.

--- backend/database/migrations/2024_10_29_084005_create_users_table.php
+++ backend/database/migrations/2024_10_29_084005_create_users_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('role', ['admin', 'recruiter', 'candidate']);
             $table->timestamps();
         });
     }
