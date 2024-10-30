d7d34cd596ac475f4e9930d7b73b5bc22c0ea847
nunestiagorn <nunestiagorn@gmail.com>
2024-10-30 11:08:45 -0300

    feat:resgate do usuário do banco com sucesso.

--- backend/app/Models/Users.php
+++ backend/app/Models/Users.php
@@ -5,11 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Users extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'name',
+        'email', 
+        'password',
+        'role'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,9 +7,12 @@ use App\Http\Controllers\ApplicationsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('users', [UsersController::class, "index"]);
 Route::post('users', [UsersController::class, "store"]);
 Route::put('users/{id}', [UsersController::class, "update"]);
+Route::delete('users/{id}', [UsersController::class, "delete"]);
+Route::post('users/login', [UsersController::class, "check"]);
 
 
 Route::get('companies', [CompaniesController::class, "index"]);
