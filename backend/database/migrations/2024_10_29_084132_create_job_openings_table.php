683d75f78c836b333cc0971bb7c1ae72320573fc
nunestiagorn <nunestiagorn@gmail.com>
2024-11-04 10:49:32 -0300

    feat:tabelas backend

--- backend/database/migrations/2024_10_29_084132_create_job_openings_table.php
+++ backend/database/migrations/2024_10_29_084132_create_job_openings_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('job_openings', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('description');
+            $table->foreignId('company_id')->nullable()->constrained('companies');
             $table->timestamps();
         });
     }
